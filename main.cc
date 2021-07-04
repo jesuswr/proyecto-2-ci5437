157c5795b9e6ff888faa2fca65dd527308a6c3e8
jesuswr <jesuswahrman@gmail.com>
2021-07-04 17:42:32 -0400

    negamax and negamax alpha beta

--- main.cc
+++ main.cc
@@ -2,7 +2,7 @@
 // Universidad Simon Bolivar, 2012.
 // Author: Blai Bonet
 // Last Revision: 1/11/16
-// Modified by: 
+// Modified by:
 
 #include <iostream>
 #include <limits>
@@ -47,16 +47,16 @@ int negascout(state_t state, int depth, int alpha, int beta, int color, bool use
 int main(int argc, const char **argv) {
     state_t pv[128];
     int npv = 0;
-    for( int i = 0; PV[i] != -1; ++i ) ++npv;
+    for ( int i = 0; PV[i] != -1; ++i ) ++npv;
 
     int algorithm = 0;
-    if( argc > 1 ) algorithm = atoi(argv[1]);
+    if ( argc > 1 ) algorithm = atoi(argv[1]);
     bool use_tt = argc > 2;
 
     // Extract principal variation of the game
     state_t state;
     cout << "Extracting principal variation (PV) with " << npv << " plays ... " << flush;
-    for( int i = 0; PV[i] != -1; ++i ) {
+    for ( int i = 0; PV[i] != -1; ++i ) {
         bool player = i % 2 == 0; // black moves first!
         int pos = PV[i];
         pv[npv - i] = state;
@@ -67,25 +67,25 @@ int main(int argc, const char **argv) {
 
 #if 0
     // print principal variation
-    for( int i = 0; i <= npv; ++i )
+    for ( int i = 0; i <= npv; ++i )
         cout << pv[npv - i];
 #endif
 
     // Print name of algorithm
     cout << "Algorithm: ";
-    if( algorithm == 1 )
+    if ( algorithm == 1 )
         cout << "Negamax (minmax version)";
-    else if( algorithm == 2 )
+    else if ( algorithm == 2 )
         cout << "Negamax (alpha-beta version)";
-    else if( algorithm == 3 )
+    else if ( algorithm == 3 )
         cout << "Scout";
-    else if( algorithm == 4 )
+    else if ( algorithm == 4 )
         cout << "Negascout";
     cout << (use_tt ? " w/ transposition table" : "") << endl;
 
     // Run algorithm along PV (bacwards)
     cout << "Moving along PV:" << endl;
-    for( int i = 0; i <= npv; ++i ) {
+    for ( int i = 0; i <= npv; ++i ) {
         //cout << pv[i];
         int value = 0;
         TTable[0].clear();
@@ -96,16 +96,17 @@ int main(int argc, const char **argv) {
         int color = i % 2 == 1 ? 1 : -1;
 
         try {
-            if( algorithm == 1 ) {
-                //value = negamax(pv[i], 0, color, use_tt);
-            } else if( algorithm == 2 ) {
-                //value = negamax(pv[i], 0, -200, 200, color, use_tt);
-            } else if( algorithm == 3 ) {
-                //value = scout(pv[i], 0, color, use_tt);
-            } else if( algorithm == 4 ) {
-                //value = negascout(pv[i], 0, -200, 200, color, use_tt);
+            // quitar los 69 despues
+            if ( algorithm == 1 ) {
+                value = negamax(pv[i], 69, color, use_tt);
+            } else if ( algorithm == 2 ) {
+                value = negamax(pv[i], 69, -200, 200, color, use_tt);
+            } else if ( algorithm == 3 ) {
+                value = scout(pv[i], 69, color, use_tt);
+            } else if ( algorithm == 4 ) {
+                value = negascout(pv[i], 69, -200, 200, color, use_tt);
             }
-        } catch( const bad_alloc &e ) {
+        } catch ( const bad_alloc &e ) {
             cout << "size TT[0]: size=" << TTable[0].size() << ", #buckets=" << TTable[0].bucket_count() << endl;
             cout << "size TT[1]: size=" << TTable[1].size() << ", #buckets=" << TTable[1].bucket_count() << endl;
             use_tt = false;
@@ -118,7 +119,7 @@ int main(int argc, const char **argv) {
              << ", #expanded=" << expanded
              << ", #generated=" << generated
              << ", seconds=" << elapsed_time
-             << ", #generated/second=" << generated/elapsed_time
+             << ", #generated/second=" << generated / elapsed_time
              << endl;
     }
 
@@ -126,33 +127,66 @@ int main(int argc, const char **argv) {
 }
 
 
+// quitar depth despues, no se usa
+int negamax(state_t state, int depth, int color, bool use_tt) {
+    ++generated;
+    if (state.terminal())
+        return color * state.value();
 
-int negamax(state_t state, int depth, int color, bool use_tt = false){
-    if(state.terminal()) return color * state.value(); 
-
-    int score = - std::numeric_limits<int>::max()
-
-    //for(auto child : state){
-    //    score = max(score, - negamax(child, -color))
-    while(){
-        auto child = state.move(color, state.pos()); // ?
-        score = max(score, - negamax(child, depth + 1, -color))
+    // -300 por comodidad, cambiar despues
+    int score = -300;
+    // booleano para saber si logre moverme colocando alguna ficha
+    // o si tengo que pasar el turno al otro
+    bool moved = false;
+    for (int p : state.get_moves(color == 1)) {
+        moved = true;
+        score = max(
+                    score,
+                    -negamax(state.move(color == 1, p), depth - 1, -color, use_tt)
+                );
     }
-    
+    // si no logre moverme sigo en el mismo estado pero cambio el color
+    if (!moved)
+        score = -negamax(state, depth - 1, -color, use_tt);
+
+    ++expanded;
     return score;
 }
 
-int negamax(state_t state, int depth, int alpha, int beta, int color, bool use_tt = false){
+int negamax(state_t state, int depth, int alpha, int beta, int color, bool use_tt) {
+    ++generated;
+    if (state.terminal())
+        return color * state.value();
+
+    // -200 por comodidad, cambiar despues
+    int score = -200;
+    // booleano para saber si logre moverme colocando alguna ficha
+    // o si tengo que pasar el turno al otro
+    bool moved = false;
+    for (int p : state.get_moves(color == 1)) {
+        moved = true;
+        score = max(
+                    score,
+                    -negamax(state.move(color == 1, p), depth - 1, -beta, -alpha, -color, use_tt)
+                );
+        alpha = max(alpha, score);
+        if (alpha >= beta)
+            break;
+    }
+    // si no logre moverme sigo en el mismo estado pero cambio el color
+    if (!moved)
+        score = -negamax(state, depth - 1, -beta, -alpha, -color, use_tt);
+
+    ++expanded;
+    return score;
+}
+
+int scout(state_t state, int depth, int color, bool use_tt) {
     //
     return 0;
 }
 
-int scout(state_t state, int depth, int color, bool use_tt = false){
-    //
-    return 0;
-}
-
-int negascout(state_t state, int depth, int alpha, int beta, int color, bool use_tt = false){
+int negascout(state_t state, int depth, int alpha, int beta, int color, bool use_tt) {
     //
     return 0;
 }
